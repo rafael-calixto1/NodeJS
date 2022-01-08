bd229fe10619c0f969faec6114023f6c4d3be7c6
Rafael Calixto <calixxtorafaell@gmail.com>
2022-01-08 18:53:34 -0300

    add the fille helloworld.js and the folder calcJS

---
+++ calcJS/calc.js
@@ -0,0 +1,9 @@
+var somaFunc = require("./soma");
+var subFunc = require("./sub");
+var diviFunc = require("./divi");
+var multiFunc = require("./multi");
+
+console.log(somaFunc(13,9));
+console.log(subFunc(7,6));
+console.log(diviFunc(100,5));
+console.log(multiFunc(9,4));
---
+++ helloworld.js
@@ -0,0 +1,2 @@
+var nome = "Rafael", numero = 9
+console.log("Hello World", numero, nome);
