36359969c3f39069160aba90eb17c4b1b5c85d83
Rafael Calixto <calixxtorafaell@gmail.com>
2022-01-09 21:53:13 -0300

    criei rotas dinâmicas através de parametros

--- Express/index.js
+++ Express/index.js
@@ -15,6 +15,20 @@ app.get("/blog", function(req, res){
      res.send("em vindo ao meu blog");
 })
 
+//Para criarmos um parametro, usamos "/:", quando criamos um parametro, necessáriamente precisamos passar um parametro, pode ser criado mais de um paremetro deste modo: "/:nome/:cargo"
+//localhost:8081/ola/rafael/desenvolvedor
+app.get('/form/:nome/:cargo', function(req, res){
+     res.send(req.params)
+})
+
+//chamando o nome através do parametro
+app.get('/ola/:nome/:cargo/:cor', function(req, res) {
+     res.send("<p>Ola " + req.params.nome+"</p>"+
+     "<p>Ola " + req.params.cargo+"</p>"+
+     "<p>Ola " + req.params.cor+"</p>"
+     );
+}) //A função .send só pode ser chamada uma vez em cada rota
+
 //a funcao listem deve ser sempre o ultimo elmento
 // a  funcao de callback é executada sempre que algum evento acontece
 app.listen(8081, function() {
