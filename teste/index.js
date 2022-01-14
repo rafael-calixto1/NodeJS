77e0d3c2fddecaa8a24d5e8516eba056daa1b692
Rafael Calixto <calixxtorafaell@gmail.com>
2022-01-14 20:53:08 -0300

    resolução do erro devido à incompatibilidade com a versão do handlaber

--- teste/index.js
+++ teste/index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
-const handlebars = require('expree-handlebars')
+const handlebars = require('express-handlebars')
 const Sequelize = require('sequelize');
+
 // Config
     //Template Engine
-        app.Engine('handlebars', handlebars({defaultLayout: 'main'}))
-        app.set('view engine', 'handlebars')
-    //Conexão com o banco de dados MySQL
-        const sequelize = new Sequelize('sequelizeTeste', 'root', '1234', {
-            host: "localhost",
-            dialect: 'mysql'
-        });
+    const hbs = handlebars.create({
+        defaultLayout: 'main'
+    })
+    app.engine('handlebars',() => hbs)
+    app.set('view engine', 'handlebars');
+
+//Conexão com o banco de dados MySQL
+    const sequelize = new Sequelize('sequelizeTeste', 'root', '1234', {
+        host: "localhost",
+        dialect: 'mysql'
+    });
+
+        app.get('/cad', function(req, res) {
+            res.send('ROTA de cadastro POSTS')
+        })
+
+
+
 
 app.listen(8081, function(){
     console.log("O servidor esta rodando na url localhost:8081")
